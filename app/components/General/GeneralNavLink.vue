a3533fbffabd59dc66148225540cacc16158c10e
amillanbc <amillan@bluecorela.com>
2025-10-28 11:37:25 -0500

    Fix: Refactor conditional classes

--- app/components/General/GeneralNavLink.vue
+++ app/components/General/GeneralNavLink.vue
@@ -2,20 +2,19 @@
   <ULink class="text-stone-500" :to="link.to">
     <div
       class="flex flex-row items-center gap-2 hover:bg-stone-100 dark:hover:bg-stone-800 px-2 py-1 rounded-lg"
-      :class="link.active ? 'bg-stone-100  dark:bg-stone-800 inter-500' : ''"
+      :class="{ 'bg-stone-100 dark:bg-stone-800 inter-500': link.active }"
     >
       <UIcon
         :name="link.icon!"
-        :class="
-          link.active
-            ? 'bg-linear-to-b from-[#e390b9] to-[#718add] text-transparent fill-current'
-            : ''
-        "
+        :class="{
+          'bg-linear-to-b from-[#e390b9] to-[#718add] text-transparent fill-current':
+            link.active,
+        }"
       />
       <div
-        :class="
-          link.active ? 'am-gradient-text bg-clip-text text-transparent' : ''
-        "
+        :class="{
+          'am-gradient-text bg-clip-text text-transparent': link.active,
+        }"
       >
         {{ link.label }}
       </div>
