4a55d82dcfac8b7936e1605dff9963bdb17c6f08
amillanbc <amillan@bluecorela.com>
2025-10-30 17:34:37 -0500

    Fix: Add sendgrid to lynkco project

--- app/composables/DataLynkCoComp.ts
+++ app/composables/DataLynkCoComp.ts
@@ -13,7 +13,7 @@ export default function lynkcoComp() {
       "A web platform for exclusive event invitations, sending email invites and managing access via QR codes and on-site scanning.",
     descriptionLong:
       "This project is a web platform designed for exclusive events, such as a car brand launch. Invitations are sent directly via email to selected guests, and access is securely managed with QR codes scanned at the venue. The system ensures smooth check-in and a premium experience for attendees while providing organizers full control over guest access.",
-    techStack: ["Vue", "Quasar", "Firebase"],
+    techStack: ["Vue", "Quasar", "Firebase", "Sendgrid"],
     url: "https://lynkco-panama.com/",
     year: "2025",
   } as ProjectData;
