4a64db87101e6c78d8444c68899c2251df8dc499
amillanbc <amillan@bluecorela.com>
2025-11-03 16:53:55 -0500

    Test: env vars

---
+++ server/api/test.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  return {
+    success: true,
+    message: {
+      toEmail: config.sendgridToEmail,
+      fromEmail: config.sendgridFromEmail,
+      templateId: config.sendgridTemplateId,
+      apiKey: config.sendgridApiKey,
+    },
+  };
+});
